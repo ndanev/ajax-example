9b4bdbf949e066bb10804ac37886cbaa3738d4c2
Nemanja Danev <nemanja.danev.93@gmail.com>
2018-04-25 22:30:19 -0400

    working ajax and json

--- myfrist.js
+++ myfrist.js
@@ -3,6 +3,9 @@ fs = require('fs');
 
 http.createServer(function (req, res) {
     console.log(req.url);
+    res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
+    res.setHeader("Pragma", "no-cache"); // HTTP 1.0.
+    res.setHeader("Expires", "0"); // Proxies.
     if (req.url == "/") {
         console.log('bla bla..');
         fs.readFile('./index.html', function (err, html) {
@@ -17,5 +20,11 @@ http.createServer(function (req, res) {
     if (req.url == "/data") {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end('Hello World!');
+    }
+    if (req.url == "/standings") {
+        //res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.setHeader("Content-Type", "application/json");
+        res.writeHead(200);
+        res.end('["Raptors","Celtics","76ers","Cavaliers","Pacers"]');
     }
 }).listen(80);
